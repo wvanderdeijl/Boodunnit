7e61139faaee535f8dc31fb99fc1ca10f8c81a02
Nienke <nm.overmeer@gmail.com>
2020-10-26 16:14:48 +0100

    Added input F,G,H for Posses, scare and levitate behaviour

---
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        //Posses behaviour
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            print("Key F was hit");
+        }
+
+        //Scare behaviour
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            print("Key G was hit");
+        }
+
+        //Levitate behaviour
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            print("Key H was hit");
+        }
+    }
+}

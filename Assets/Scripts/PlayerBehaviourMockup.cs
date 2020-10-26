62fdac620211c78cd3cb53e21f4d553dcd402ada
Jesse Minneboo <s1111192@student.hsleiden.nl>
2020-10-26 14:59:52 +0100

    Added PlayerBehaviourMockup

---
+++ Assets/Scripts/PlayerBehaviourMockup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerBehaviourMockup : MonoBehaviour
+{
+    [SerializeField] private LevitateBehaviour _levitateBehaviour;
+    //[SerializeField] private TerrifyBehaviour _terrifyBehaviour;
+
+    private void Update()
+    {
+        throw new NotImplementedException();
+    }
+}

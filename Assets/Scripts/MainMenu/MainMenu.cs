d9278be24c5aad4dbc3ee357fb7eaac00377c036
ryanr <ryanrooijakkers@hotmail.com>
2020-10-27 16:32:48 +0100

    Added the main menu

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        // Todo start the game
+    }
+
+    public void LoadGame()
+    {
+        // Todo load the game
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}

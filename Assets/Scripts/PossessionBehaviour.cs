55bd1e58843a3669e5d6e7265f06705de063e9c9
ryanr <ryanrooijakkers@hotmail.com>
2020-10-27 10:54:13 +0100

    Removed empty start method

--- Assets/Scripts/PossessionBehaviour.cs
+++ Assets/Scripts/PossessionBehaviour.cs
@@ -12,12 +12,6 @@ public class PossessionBehaviour : MonoBehaviour
     private GameObject _possessionTarget;
     private bool _isPossessing = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {

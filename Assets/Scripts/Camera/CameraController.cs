14571bc2aa69a1799fd017e036ec67c8b1bf5038
uitrich <fishynathan@gmail.com>
2020-10-29 16:35:17 +0100

    vertical camera obstruction movement

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -8,8 +8,23 @@ public class CameraController : MonoBehaviour
     /// <summary>
     /// Describes the speed at which the camera will move to a given new position along a circular axis
     /// </summary>
-    public float Sensitivity = 5f; 
-    public float Distance = 7f;
+    public float Sensitivity = 5f;
+
+    public float MaxDistance = 7;
+    public float MinDistance = 1.255763f;
+    public float Distance
+    {
+        get { return _distance; }
+        set
+        {
+            if (value > MaxDistance) value = MaxDistance;
+            if (value < MinDistance) value = MinDistance;
+            _distance = value;
+        }
+    }
+    [SerializeField]
+    private float _distance;
+    
     public float RotationSpeed = 20f;
 
     public Transform CameraRotationTarget;
@@ -17,17 +32,26 @@ public class CameraController : MonoBehaviour
     private Vector3 _pointToSlerpTo;
     private float _angle;
     private Vector2 _rotationInput;
-    
+    [SerializeField]
+    private float _scrollingInput;
+
+    private bool _scrollZoomActivation;
+
+    private float _minElevationOrigin;
+    private float _maxElevationOrigin;
+    public float MaxElevation = 8f;
+    public float MinElevation = -0.5f;
     public float ElevationRange
     {
         get { return _elevationRange;}
         set
         {
-            if (value > 8f) value = 8f;
-            if (value < -2f) value = -2f;
+            if (value > MaxElevation) value = MaxElevation;
+            if (value < MinElevation) value = MinElevation;
             _elevationRange = value;
         }
     }
+    [SerializeField]
     private float _elevationRange = 2f;
 
     private void Awake()
@@ -35,15 +59,19 @@ public class CameraController : MonoBehaviour
         _pointToSlerpTo = transform.position;
         _angle = Vector3.Angle(CameraRotationTarget.position, _pointToSlerpTo);
         Cursor.lockState = CursorLockMode.Locked;
+        Distance = MaxDistance;
+        _minElevationOrigin = MinElevation;
+        _maxElevationOrigin = MaxElevation;
     }
 
     private void Update()
     {
         _rotationInput.x = Input.GetAxis("Mouse X");
         _rotationInput.y = -Input.GetAxis("Mouse Y");
+        _scrollingInput = -Input.GetAxis("Mouse ScrollWheel");
         
         float angleOffset = 0;
-        _pointToSlerpTo.y = ElevationRange;
+        _pointToSlerpTo.y = ElevationRange + CameraRotationTarget.position.y;;
 
         Vector3 camPos = transform.position;
         Vector2 plainCamPos = new Vector2(camPos.x, camPos.z);
@@ -76,6 +104,48 @@ public class CameraController : MonoBehaviour
         {
             ElevateCamera(_rotationInput.y);
         }
+
+        if (_scrollingInput != 0)
+        {
+            _scrollZoomActivation = true;
+            ScrollZoom(_scrollingInput * 5 * Time.deltaTime * RotationSpeed);
+        }
+        else if (!_scrollZoomActivation && Distance < MaxDistance)
+        {
+            ScrollZoom(1f);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (Physics.Raycast(CameraRotationTarget.position, (transform.position - CameraRotationTarget.position).normalized,out RaycastHit raycastHit,
+            Distance ))
+        {
+            ScrollZoom(-Vector3.Distance(transform.position, raycastHit.point));
+            _scrollZoomActivation = false;
+            if (raycastHit.point.y <= CameraRotationTarget.position.y || CameraRotationTarget.position.y > transform.position.y)
+            {
+                ElevateCamera(30f);
+                Vector3 newPoint = raycastHit.point;
+                newPoint.y += 0.1f;
+                transform.position = newPoint;
+                _scrollZoomActivation = true;
+            }
+            else if (raycastHit.point.y > CameraRotationTarget.position.y +
+                CameraRotationTarget.GetComponent<Collider>().bounds.size.y)
+            {
+                ElevateCamera(-30f);
+                _scrollZoomActivation = true;
+            }
+        }
+    }
+
+    private void ScrollZoom(float zoomAmount)
+    {
+        Distance += zoomAmount;
+        MinElevation = _minElevationOrigin * Distance / 7;
+        MaxElevation = _maxElevationOrigin * Distance / 7;
+        RotateCamera(0);
     }
 
 

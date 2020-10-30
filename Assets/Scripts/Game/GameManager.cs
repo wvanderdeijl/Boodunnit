1053ce9daeea0c422bb5117d50ba4b50a46ff1cf
Phone-game <darylvdberg@gmail.com>
2020-10-30 15:38:05 +0100

    Expanded functionality of SaveHandler

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Button SaveButton;
+    public Button LoadButton;
+    public Scene Scene;
+
+    private void Awake()
+    {
+        SaveHandler.Instance.StartNewGame();
+        Scene = SceneManager.GetActiveScene();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SaveKnaapPosition()
+    {
+        GameObject gameObject = GameObject.Find("Knaap");
+        if (gameObject)
+        {
+            float[] position = new float[3];
+            position[0] = gameObject.transform.position.x;
+            position[1] = gameObject.transform.position.y;
+            position[2] = gameObject.transform.position.z;
+            SaveHandler.Instance.UpdateSaveGame(Scene.name, gameObject.name, position);
+        }
+    }
+
+    public void LoadSaveState()
+    {
+        Dictionary<string, object> objects = SaveHandler.Instance.LoadGameObjects(Scene.name);
+    }
+}
--- Assets/Scripts/Game/SaveSystem/SaveHandler.cs
+++ Assets/Scripts/Game/SaveSystem/SaveHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SaveHandler
@@ -23,7 +24,7 @@ public class SaveHandler
 
     public void StartNewGame()
     {
-        List<object> saveGameData = new List<object>();
+        List<Dictionary<string, object>> saveGameData = new List<Dictionary<string, object>>();
         string saveGameDataString = JsonConvert.SerializeObject(saveGameData);
         PlayerPrefs.SetString(_saveGame, saveGameDataString);
 
@@ -36,19 +37,95 @@ public class SaveHandler
         PlayerPrefs.Save();
     }
 
-    public string LoadGame()
+    public List<Dictionary<string, object>> LoadGame()
     {
-        return PlayerPrefs.GetString(_saveGame);
+        return DezerializeSaveGameList();
     }
 
-    public void UpdateSaveGame<T>(T dataToAddToSaveGame)
+    public Dictionary<string, object> LoadGame(string sceneName)
     {
-        string saveGameString = PlayerPrefs.GetString(_saveGame);
-        List<object> saveGameData = JsonConvert.DeserializeObject<List<object>>(saveGameString);
+        List<Dictionary<string, object>> saveGame = DezerializeSaveGameList();
+        return GetSceneFromSaveGameList(saveGame, sceneName);
+    }
 
-        saveGameData.Add(dataToAddToSaveGame);
-        saveGameString = JsonConvert.SerializeObject(saveGameData);
+    public Dictionary<string, object> LoadGameObjects(string sceneName)
+    {
+        List<Dictionary<string, object>> saveGame = DezerializeSaveGameList();
+        Dictionary<string, object> scene = GetSceneFromSaveGameList(saveGame, sceneName);
+        Dictionary<string, object> objects = JsonConvert.DeserializeObject<Dictionary<string, object>>(scene[sceneName].ToString());
 
+        return objects;
+    }
+
+    /**
+     * sceneName : string -> I expect you to use Unity to get name of current scene.
+     * nameOfObject : string -> Name of the object / gameobject you want data to be saved.
+     *                          I want you to use very specific names for each gameobject.
+     *                          If it's a gameobject, use Unity way to get name of gameobject
+     * 
+     * dataToSave : object -> Data you want to save.
+     **/
+    public void UpdateSaveGame(string sceneName, string nameOfObject, object dataToSave)
+    {
+        List<Dictionary<string, object>> saveGame = DezerializeSaveGameList();
+        /**
+         * Hoofd lijst:
+         *  - Dictionary<Scene 1, object to Save>
+         *          - Dictionary<door, true>
+         *          - Dictionary<Politieman, Dictionary<string, object>
+         *  - Dictionary<Scene 2, objects to save>
+         *  
+         *  Dict bestaat
+         *      - Haal dictionary van scene op
+         *      - Voeg nieuwe data toe.
+         *      
+         *   ----------------------------------
+         *   
+         *   - Scene 2
+         *      - Politieman
+         *          - Positie -> x,y,z
+         *          - hasKey -> false
+         *   
+         *   ------------------------------------
+         *   1. Bestaat de scene? Ja: kijk op object bestaat: Ja? update.
+         *   2. Bestaat de scene? Nee: Dan kan het object ook niet bestaat dus voeg toe.
+         **/
+
+        bool hasScene = saveGame.Any(scene => scene.ContainsKey(sceneName));
+        if (hasScene)
+        {
+            Dictionary<string, object> scene = new Dictionary<string, object>();
+
+            // Refector later to be more effiecient
+            foreach(Dictionary<string, object> sceneInList in saveGame)
+            {
+                if (sceneInList.ContainsKey(sceneName))
+                {
+                    scene = sceneInList;
+                }
+            }
+
+            bool isObjectInDictionary = scene.Any(objectInDictionary => objectInDictionary.Key == nameOfObject);
+            if (isObjectInDictionary)
+            {
+                scene[nameOfObject] = dataToSave;
+            } else
+            {
+                scene.Add(nameOfObject, dataToSave);
+            }
+
+            return;
+        }
+
+        Dictionary<string, object> SceneDictionary = new Dictionary<string, object>();
+        Dictionary<string, object> saveData = new Dictionary<string, object>();
+
+        saveData.Add(nameOfObject, dataToSave);
+        SceneDictionary.Add(sceneName, saveData);
+
+        saveGame.Add(SceneDictionary);
+
+        string saveGameString = JsonConvert.SerializeObject(saveGame);
         PlayerPrefs.SetString(_saveGame, saveGameString);
         PlayerPrefs.Save();
     }
@@ -68,5 +145,25 @@ public class SaveHandler
     {
         return PlayerPrefs.GetString("PlayerSettings") != null ||
            PlayerPrefs.GetString("PlayerSettings") != "";
+    }
+
+    private List<Dictionary<string, object>> DezerializeSaveGameList()
+    {
+        string saveGameString = PlayerPrefs.GetString(_saveGame);
+        List<Dictionary<string, object>> saveGame = JsonConvert.DeserializeObject<List<Dictionary<string, object>>>(saveGameString);
+        return saveGame;
+    }
+
+    private Dictionary<string, object> GetSceneFromSaveGameList(List<Dictionary<string, object>> saveGame, string sceneName)
+    {
+        foreach (Dictionary<string, object> sceneInList in saveGame)
+        {
+            if (sceneInList.ContainsKey(sceneName))
+            {
+                return sceneInList;
+            }
+        }
+
+        return null;
     }
 }

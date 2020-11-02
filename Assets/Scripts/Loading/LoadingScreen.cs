70414bb9cce33b8747bcb0ecdaafd3b8ef81843c
Jesse Minneboo <s1111192@student.hsleiden.nl>
2020-11-02 16:35:06 +0100

    Added loading screen

---
+++ Assets/Scripts/Loading/LoadingScreen.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class LoadingScreen : MonoBehaviour
+{
+    public Slider LoadingBar;
+    public Text HintText;
+    public string[] HintTextArray;
+
+    private float _minimumLoadingTime = 2f;
+
+    private void Awake()
+    {
+        ShowRandomHintInLoadScreen();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(LoadLevelAsynchronously("LevitateScene"));
+    }
+
+    private IEnumerator LoadLevelAsynchronously(string sceneName)
+    {
+        AsyncOperation asyncLevelLoad = SceneManager.LoadSceneAsync(sceneName);
+
+        asyncLevelLoad.allowSceneActivation = false;
+
+        while (!asyncLevelLoad.isDone)
+        {
+            float progress = Mathf.Clamp01(asyncLevelLoad.progress / 0.9f);
+            UpdateLoadingBarValue(progress);
+            
+            if (asyncLevelLoad.progress >= 0.9f)
+            {
+                yield return new WaitForSeconds(_minimumLoadingTime);
+                asyncLevelLoad.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+    }
+    
+   
+
+    private void UpdateLoadingBarValue(float progress)
+    {
+        LoadingBar.value = progress;
+    }
+
+    private void ShowRandomHintInLoadScreen()
+    {
+        HintText.text = GetRandomHintFromArray();
+    }
+
+    private string GetRandomHintFromArray()
+    {
+        int randomIndex = Random.Range (0, HintTextArray.Length);
+        return HintTextArray[randomIndex];
+    }
+}
---
+++ Assets/Scripts/Loading/SceneTransitionHandler.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionHandler : MonoBehaviour
+{
+    public static SceneTransitionHandler Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new SceneTransitionHandler();
+            }
+            
+            return _instance;
+        }
+    }
+    
+    private static SceneTransitionHandler _instance;
+
+    public void GoToScene(string sceneName)
+    {
+        // niet naar loading screen, maar volgende screen. Loading screen wordt 
+        // automatisch ingeladen.
+        // todo
+        SceneManager.LoadScene(sceneName);
+    }
+
+    private void Start()
+    {
+        GoToScene("LoadingScene");
+    }
+}

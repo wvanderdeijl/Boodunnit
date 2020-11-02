92e9fe7d25200ff6f6eded12a6af0a35649967b1
Phone-game <darylvdberg@gmail.com>
2020-11-02 16:34:13 +0100

    Refactored to use different player prefs for each scene

--- Assets/Scripts/Game/SaveSystem/SaveHandler.cs
+++ Assets/Scripts/Game/SaveSystem/SaveHandler.cs
@@ -1,11 +1,12 @@
 ﻿using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// Singleton class. To access the Instance you type: SaveHandler.Instance.MethodYouWantToCall();
-/// 
 /// </summary>
 public class SaveHandler
 {
@@ -23,131 +24,60 @@ public class SaveHandler
         }
     }
 
-    private readonly string _saveGame = "Save";
     private readonly string _playerSettings = "PlayerSettings";
-
-    /// <summary>
-    /// This method is used to start a new game. It'll create a new list that contains the scene and object data.
-    /// IMPORTANT: You'll probably want to call DeleteSaveGame() first to remove the previous save.
-    /// </summary>
-    public void StartNewGame()
-    {
-        List<Dictionary<string, object>> saveGameData = new List<Dictionary<string, object>>();
-        string saveGameDataString = JsonConvert.SerializeObject(saveGameData);
-        PlayerPrefs.SetString(_saveGame, saveGameDataString);
-
-        PlayerPrefs.Save();
-    }
 
     /// <summary>
     /// This method will remove the current save game.
     /// </summary>
     public void DeleteSaveGame()
     {
-        PlayerPrefs.DeleteKey(_saveGame);
-        PlayerPrefs.Save();
+        PlayerPrefs.DeleteAll();
     }
 
-    /// <summary>
-    /// This method return the save game list that contains all scenes
-    /// IMPORTANT: Only call this method when you really need the entire save game list.
-    /// </summary>
-    /// <returns>List with a dictionary per scene.</returns>
-    public List<Dictionary<string, object>> LoadGame()
+    public void SaveGameProperty(string nameOfGameObject, string nameOfProperty, object propertyValue)
     {
-        return DezerializeSaveGameList();
-    }
+        string sceneName = SceneManager.GetActiveScene().name;
+        string unqiueKey = nameOfGameObject + "_" + nameOfProperty;
 
-    /// <summary>
-    /// This method returns the dictionary from a specific scene.
-    /// </summary>
-    /// <param name="sceneName">Name of the scene. Please use the unity method to get the scene name to avoid any problems.</param>
-    /// <returns>Dictionary of the scene</returns>
-    public Dictionary<string, object> LoadGame(string sceneName)
-    {
-        List<Dictionary<string, object>> saveGame = DezerializeSaveGameList();
-        return GetSceneFromSaveGameList(saveGame, sceneName);
-    }
+        string scene = PlayerPrefs.GetString(sceneName);
 
-    /// <summary>
-    /// This method returns the objects in the scene directory.
-    /// </summary>
-    /// <param name="sceneName">Name of the scene. Please use the unity method to get the scene name to avoid any problems.</param>
-    /// <returns>Dictionary of the objects within a specific scene.</returns>
-    public Dictionary<string, object> LoadGameObjects(string sceneName)
-    {
-        List<Dictionary<string, object>> saveGame = DezerializeSaveGameList();
-        Dictionary<string, object> scene = GetSceneFromSaveGameList(saveGame, sceneName);
-        Dictionary<string, object> objectsInScene = JsonConvert.DeserializeObject<Dictionary<string, object>>(scene[sceneName].ToString());
-
-        return objectsInScene;
-    }
-
-    /// <summary>
-    /// This method loads a specific object value in a specific scene.
-    /// </summary>
-    /// <typeparam name="T">You know how you saved it, if you want to load it, give the type of object</typeparam>
-    /// <param name="sceneName">Name of the scene. Please use the unity method to get the scene name to avoid any problems.</param>
-    /// <param name="nameOfObject">Name of the object you want to load.</param>
-    /// <returns>Value of given type</returns>
-    public T LoadGameObject<T>(string sceneName, string nameOfObject)
-    {
-        List<Dictionary<string, object>> saveGame = DezerializeSaveGameList();
-        Dictionary<string, object> scene = GetSceneFromSaveGameList(saveGame, sceneName);
-        Dictionary<string, object> objectsInScene = JsonConvert.DeserializeObject<Dictionary<string, object>>(scene[sceneName].ToString());
-
-        return objectsInScene.ContainsKey(nameOfObject) ? JsonConvert.DeserializeObject<T>(objectsInScene[nameOfObject].ToString()) : default;
-    }
-
-    /// <summary>
-    /// This method is used to save the game. For now it's used as an "Autosave"'
-    /// Call this method everytime the player did something that needs to be remembered.
-    /// </summary>
-    /// <param name="sceneName">Name of the scene. Please use the unity method to get the scene name to avoid any problems.</param>
-    /// <param name="nameOfObject">Name of the object you want to save.</param>
-    /// <param name="dataToSave">Data you want to save</param>
-    public void UpdateSaveGame(string sceneName, string nameOfObject, object dataToSave)
-    {
-        List<Dictionary<string, object>> saveGame = DezerializeSaveGameList();
-       
-        bool hasScene = saveGame.Any(scene => scene.ContainsKey(sceneName));
-        if (hasScene)
+        if (scene != null && !scene.Equals(""))
         {
-            Dictionary<string, object> scene = new Dictionary<string, object>();
+            Dictionary<string, object> propertiesInScene = JsonConvert.DeserializeObject<Dictionary<string, object>>(scene);
 
-            foreach(Dictionary<string, object> sceneInList in saveGame)
+            if (propertiesInScene.ContainsKey(unqiueKey))
             {
-                if (sceneInList.ContainsKey(sceneName))
-                {
-                    scene = sceneInList;
-                }
-            }
-
-            bool isObjectInDictionary = scene.Any(objectInDictionary => objectInDictionary.Key == nameOfObject);
-            if (isObjectInDictionary)
-            {
-                scene[nameOfObject] = dataToSave;
+                propertiesInScene[unqiueKey] = propertyValue;
             } else
             {
-                scene.Add(nameOfObject, dataToSave);
+                propertiesInScene.Add(unqiueKey, propertyValue);
             }
 
-            return;
+        } else
+        {
+            Dictionary<string, object> propertiesInScene = new Dictionary<string, object>();
+            propertiesInScene.Add(unqiueKey, propertyValue);
+            PlayerPrefs.SetString(sceneName, JsonConvert.SerializeObject(propertiesInScene));
         }
 
-        Dictionary<string, object> SceneDictionary = new Dictionary<string, object>();
-        Dictionary<string, object> saveData = new Dictionary<string, object>();
-
-        saveData.Add(nameOfObject, dataToSave);
-        SceneDictionary.Add(sceneName, saveData);
-
-        saveGame.Add(SceneDictionary);
-
-        string saveGameString = JsonConvert.SerializeObject(saveGame);
-        PlayerPrefs.SetString(_saveGame, saveGameString);
         PlayerPrefs.Save();
     }
 
+    public T GetPropertyValueFromUniqueKey<T>(string nameOfGameObject, string nameOfProperty)
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+        string uniqueKey = nameOfGameObject + "_" + nameOfProperty;
+
+        Dictionary<string, object> propertiesInScene = JsonConvert.DeserializeObject<Dictionary<string, object>>(PlayerPrefs.GetString(sceneName));
+
+        if (propertiesInScene.ContainsKey(uniqueKey))
+        {
+            return (T)propertiesInScene[uniqueKey];
+        }
+
+        return default;
+    }
+    
     /// <summary>
     /// This method is used to save the player settings
     /// </summary>
@@ -178,38 +108,5 @@ public class SaveHandler
     {
         return PlayerPrefs.GetString("PlayerSettings") != null ||
            PlayerPrefs.GetString("PlayerSettings") != "";
-    }
-
-    /// <summary>
-    /// This method is used when you still need to dezerialize data.
-    /// This is because, if you have a complex object, not everything will be dezerialized right away.
-    /// </summary>
-    /// <typeparam name="T">Type of data</typeparam>
-    /// <param name="dataToDezerialize">JSON string of the data you want to dezerialize</param>
-    /// <returns>Object with given type.</returns>
-    public T DezerializeGameData<T>(string dataToDezerialize)
-    {
-        T dezerializedData = JsonConvert.DeserializeObject<T>(dataToDezerialize);
-        return dezerializedData != null ? dezerializedData : default;
-    }
-
-    private List<Dictionary<string, object>> DezerializeSaveGameList()
-    {
-        string saveGameString = PlayerPrefs.GetString(_saveGame);
-        List<Dictionary<string, object>> saveGame = JsonConvert.DeserializeObject<List<Dictionary<string, object>>>(saveGameString);
-        return saveGame;
-    }
-
-    private Dictionary<string, object> GetSceneFromSaveGameList(List<Dictionary<string, object>> saveGame, string sceneName)
-    {
-        foreach (Dictionary<string, object> sceneInList in saveGame)
-        {
-            if (sceneInList.ContainsKey(sceneName))
-            {
-                return sceneInList;
-            }
-        }
-
-        return null;
     }
 }

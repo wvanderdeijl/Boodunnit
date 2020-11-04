560f0a44bc99f069f8ee30d037a3a5294816c344
TimvHal <55191080+TimvHal@users.noreply.github.com>
2020-11-04 12:04:05 +0100

    Added climbing and rat
    - Added climbing behaviour
    - Added rat

---
+++ Assets/Scripts/ClimbBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClimbBehaviour : MonoBehaviour
+{
+
+    public float MinimumStamina { get; set; }
+    public float MaximumStamina { get; set; }
+    public float CurrentStamina { get; set; }
+    public float Speed { get; set; }
+    
+    public bool IsClimbing;
+    public bool CanClimb;
+    
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private float _wallDetectionRadius = 2f;
+
+    private void Awake()
+    {
+        IsClimbing = false;
+    }
+
+    //Call this method in an ability function (f.e. UseFirstAbility() in IEntity).
+    public void ToggleClimb()
+    {
+        Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, 1f,
+            LayerMask.NameToLayer("Climbable"));
+        
+        //TODO rotate transform.down towards wall
+        IsClimbing = !IsClimbing;
+    }
+
+    public void Climb(Vector3 direction)
+    {
+        direction = direction.normalized;
+        _rigidbody.velocity = direction * Speed;
+    }
+    
+    
+}
---
+++ Assets/Scripts/Entities/RatBehaviour.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Enums;
+using Interfaces;
+using UnityEngine;
+
+public class RatBehaviour : BaseMovement, IEntity, IPossessable
+{
+    public float FearThreshold { get; set; }
+    public float FearDamage { get; set; }
+    public float FaintDuration { get; set; }
+    public EmotionalState EmotionalState { get; set; }
+    public Dictionary<Type, float> ScaredOfGameObjects { get; set; }
+
+    [SerializeField] private ClimbBehaviour _climbBehaviour;
+
+    private void Awake()
+    {
+        _climbBehaviour.MinimumStamina = 0f;
+        _climbBehaviour.MaximumStamina = 10f;
+        _climbBehaviour.CurrentStamina = 10f;
+        _climbBehaviour.Speed = 5f;
+    }
+
+    private void Update()
+    {
+        //if (_climbBehaviour.IsClimbing) _climbBehaviour.Climb(direction);
+        //else MoveEntityInDirection(direction);
+    }
+
+    public void DealFearDamage(float amount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerator CalmDown()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Faint()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CheckSurroundings()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UseFirstAbility()
+    {
+        _climbBehaviour.ToggleClimb();
+    }
+
+    public void UseSecondAbility()
+    {
+        throw new NotImplementedException();
+    }
+}

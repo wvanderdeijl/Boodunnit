66e65a02de51d59ceb2177bcc21e9a362454cb24
Phone-game <darylvdberg@gmail.com>
2020-11-04 14:45:56 +0100

    Extended functionality of saveHandler.

--- Assets/Scripts/Game/SaveSystem/DataContainers/PlayerData.cs
+++ Assets/Scripts/Game/SaveSystem/DataContainers/PlayerData.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerData
+public class PlayerData : BaseDataContainer
 {
     public float PlayerPositionX, PlayerPositionY, PlayerPositionZ;
     public float PlayerRotationX, PlayerRotationY, PlayerRotationZ;
+
+    public override void ValidateData()
+    {
+        throw new System.NotImplementedException();
+    }
 }

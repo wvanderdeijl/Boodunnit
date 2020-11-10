8cf8e73a2b2a407a40328771b8f1ebce6cecd066
TimvHal <55191080+TimvHal@users.noreply.github.com>
2020-11-10 15:35:26 +0100

    Made Gliding work
    - Added GlideBehaviour.
    - Added Bird Entity.

---
+++ Assets/Scripts/Entities/Abilities/GlideBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Interfaces;
+using UnityEngine;
+
+public class GlideBehaviour : MonoBehaviour
+{
+    public bool IsGliding { get; set; }
+
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private float _glideGravity = 1f;
+
+    private void Update()
+    {
+        _rigidbody.angularVelocity = Vector3.zero;
+        if(IsGliding) ApplyLoweredGravity();
+    }
+
+    public void ToggleGlide()
+    {
+        if (!IsGliding)
+        {
+            EnableGlide();
+            return;
+        }
+        DisableGlide();
+    }
+
+    private void EnableGlide()
+    {
+        IsGliding = true;
+        _rigidbody.useGravity = false;
+    }
+
+    private void DisableGlide()
+    {
+        IsGliding = false;
+        _rigidbody.useGravity = true;
+    }
+
+    private void ApplyLoweredGravity()
+    {
+        _rigidbody.velocity =  new Vector3(_rigidbody.velocity.x, -_glideGravity, _rigidbody.velocity.z);
+    }
+}
--- Assets/Scripts/Entities/BaseMovement.cs
+++ Assets/Scripts/Entities/BaseMovement.cs
@@ -6,16 +6,11 @@ public abstract class BaseMovement : MonoBehaviour
     public float Speed;
 
     private float _rotationSpeed = 10f;
-    private float _gravity = 9.81f;
-    private void FixedUpdate()
-    {
-        Rigidbody.AddForce(Vector3.down * (_gravity * Rigidbody.mass));//ToDo: Why are you multiplying by .mass???, you can just pass a param to tell this method to ignore mass if you want that?
-    }
 
-    public void MoveEntityInDirection(Vector3 direction)
+    public void MoveEntityInDirection(Vector3 direction, float speed)
     {
         float yVelocity = Rigidbody.velocity.y;
-        Rigidbody.velocity = direction.normalized * Speed;
+        Rigidbody.velocity = direction.normalized * speed;
         Rigidbody.velocity += new Vector3(0f, yVelocity, 0f);
 
         if (direction != Vector3.zero)
@@ -23,5 +18,10 @@ public abstract class BaseMovement : MonoBehaviour
             transform.rotation = Quaternion.Lerp(transform.rotation, 
                 Quaternion.LookRotation(direction.normalized), Time.deltaTime * _rotationSpeed);
         }
+    }
+
+    public void MoveEntityInDirection(Vector3 direction)
+    {
+        MoveEntityInDirection(direction, Speed);
     }
 }

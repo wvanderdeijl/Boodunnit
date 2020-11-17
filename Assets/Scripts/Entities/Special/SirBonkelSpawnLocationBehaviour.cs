2e5a249226224b036678b081881e97f1a51c3cdc
ryanr <ryanrooijakkers@hotmail.com>
2020-11-17 15:07:23 +0100

    Made Sir bonkel and spawnlocations for sir bonkel

---
+++ Assets/Scripts/Entities/Special/SirBonkelSpawnLocationBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SirBonkelSpawnLocationBehaviour : MonoBehaviour
+{
+    public Transform Spawnpoint;
+
+    private SirBonkelBehaviour _sirBonkelBehaviour;
+
+    private void Awake()
+    {
+        _sirBonkelBehaviour = FindObjectOfType<SirBonkelBehaviour>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerBehaviour>() && _sirBonkelBehaviour)
+        {
+            _sirBonkelBehaviour.SpawnToNewLocation(Spawnpoint);
+        }
+    }
+}

9da88d2ab1e4a7ad82525cf6b14a7af6a021f5bc
uitrich <fishynathan@gmail.com>
2020-11-19 14:47:52 +0100

    Update BaseMovement.cs

--- Assets/Scripts/Entities/BaseMovement.cs
+++ Assets/Scripts/Entities/BaseMovement.cs
@@ -42,40 +42,46 @@ public abstract class BaseMovement : MonoBehaviour
 
     public void MoveEntityInDirection(Vector3 direction, float speed)
     {
-        //if (_hasCollidedWithWall)
-        //{
-        //    foreach (var contact in _contacts)
-        //    {
-        //        Vector3 contactDirection = (contact.point - transform.position);
+        if (_hasCollidedWithWall)
+        {
+            foreach (var contact in _contacts)
+            {
+                Vector3 contactDirection = (contact.point - transform.position);
                 
-        //        bool raycast = Physics.Raycast(transform.position, contactDirection, out RaycastHit hit,
-        //            Vector3.Distance(transform.position, contact.point) + 0.2f, ~LayerMask.GetMask("Player", "PlayerDash", "Possessable"));
-        //          if (raycast)
-        //          {
-        //            if ((hit.normal.y > 0.75 && contact.point.y < transform.position.y - Collider.bounds.size.y/2)) continue;
+                bool raycast = Physics.Raycast(transform.position, contactDirection, out RaycastHit hit,
+                    Vector3.Distance(transform.position, contact.point) + 0.2f, ~LayerMask.GetMask("Player", "PlayerDash", "Possessable"));
+                  if (raycast)
+                  {
+                    if (
+                        (hit.normal.y > 0.75 || hit.normal.y < 0) && 
+                        (contact.point.y < transform.position.y - Collider.bounds.size.y/2 
+                         || contact.point.y > transform.position.y + Collider.bounds.size.y/2)
+                        ) continue;
                     
-        //            float contactAngle = Vector3.Angle(IgnoreY(direction), IgnoreY(contactDirection));
-        //            Vector3 contactCross = Vector3.Cross(IgnoreY(direction), IgnoreY(contactDirection));
+                    float contactAngle = Vector3.Angle(IgnoreY(direction), IgnoreY(contactDirection));
+                    Vector3 contactCross = Vector3.Cross(IgnoreY(direction), IgnoreY(contactDirection));
 
-        //            if (contactCross.y < 0) contactAngle = -contactAngle;
+                    if (contactCross.y < 0) contactAngle = -contactAngle;
                     
                     
-        //            if (Math.Abs(contactAngle) > 10 && Math.Abs(contactAngle) <= 90)
-        //            {
-        //                float angle = contactAngle > 0 ? 90 : contactAngle < 0 ? -90 : 0;
-        //                direction = Quaternion.Euler(0, angle, 0) * hit.normal * (Math.Abs(contactAngle)/90);
-        //            }    
-        //            else if (Math.Abs(contactAngle) > 90)
-        //            {
-        //                continue;
-        //            }
-        //            else
-        //            {
-        //                direction = Vector3.zero;
-        //            }
-        //        }
-        //    }
-        //}
+                    if (Math.Abs(contactAngle) > 10 && Math.Abs(contactAngle) <= 90)
+                    {
+                        float angle = contactAngle > 0 ? 90 : contactAngle < 0 ? -90 : 0;
+                        direction = Quaternion.Euler(0, angle, 0) * hit.normal * (Math.Abs(contactAngle)/90);
+                        direction.y = 0;
+
+                    }    
+                    else if (Math.Abs(contactAngle) > 90)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        direction = Vector3.zero;
+                    }
+                }
+            }
+        }
         
         float yVelocity = Rigidbody.velocity.y;
         Rigidbody.velocity = direction * speed;

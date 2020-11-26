9451e178b16406084f8ea28c6173e56316941ac2
ryanr <ryanrooijakkers@hotmail.com>
2020-11-26 14:43:46 +0100

    fixed hightlight shizzles

--- Assets/QuickOutline/Scripts/Outline.cs
+++ Assets/QuickOutline/Scripts/Outline.cs
@@ -81,9 +81,26 @@ public class Outline : MonoBehaviour {
   private bool needsUpdate;
 
   void Awake() {
+    foreach (var skinnedMeshRenderer in GetComponentsInChildren<SkinnedMeshRenderer>())
+    {
+        if (skinnedMeshRenderer.sharedMesh.subMeshCount > 1)
+        {
+            skinnedMeshRenderer.sharedMesh.subMeshCount = skinnedMeshRenderer.sharedMesh.subMeshCount + 1;
+            skinnedMeshRenderer.sharedMesh.SetTriangles(skinnedMeshRenderer.sharedMesh.triangles, skinnedMeshRenderer.sharedMesh.subMeshCount - 1);
+        }
 
+    }
+
+    foreach (var meshFilter in GetComponentsInChildren<MeshFilter>())
+    {
+        if (meshFilter.sharedMesh.subMeshCount > 1)
+        {
+            meshFilter.sharedMesh.subMeshCount = meshFilter.sharedMesh.subMeshCount + 1;
+            meshFilter.sharedMesh.SetTriangles(meshFilter.sharedMesh.triangles, meshFilter.sharedMesh.subMeshCount - 1);
+        }
+    }
     // Cache renderers
-    renderers = GetComponentsInChildren<Renderer>();
+     renderers = GetComponentsInChildren<Renderer>();
 
     // Instantiate outline materials
     outlineMaskMaterial = Instantiate(Resources.Load<Material>(@"Materials/OutlineMask"));

1ba4a8ed784870021f08a00ca4c85807c46bef73
ryanr <ryanrooijakkers@hotmail.com>
2020-12-07 16:46:54 +0100

    Popups

---
+++ Assets/Scripts/UI/PopupMenu/PopupTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupTrigger : MonoBehaviour
+{
+    public Canvas Popup;
+
+    private bool _hasBeenTriggered = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (Popup != null && !_hasBeenTriggered && FindObjectOfType<PlayerBehaviour>() == other.GetComponent<PlayerBehaviour>())
+        {
+            GameManager.IsPaused = !GameManager.IsPaused;
+            GameManager.CursorIsLocked = !GameManager.CursorIsLocked;
+            Time.timeScale = GameManager.IsPaused ? 0 : 1;
+
+            Popup.gameObject.SetActive(true);
+            _hasBeenTriggered = true;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/PreTutorialPopup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PreTutorialPopup : MonoBehaviour
+{
+    [TextArea(3, 10)]
+    public string[] PopupTexts;
+    public GameObject NextButton;
+    public GameObject CloseButton;
+    public Text PopupText;
+
+    private int _textCounter = 0;
+
+    public void Awake()
+    {
+        if (PopupTexts != null && PopupTexts.Length >= 1)
+        {
+            PopupText.text = PopupTexts[0];
+        }
+        if (PopupTexts.Length > 1)
+        {
+            NextButton.SetActive(true);
+            CloseButton.SetActive(false);
+        }
+    }
+
+    public void OnNextClicked()
+    {
+        _textCounter++;
+        if (PopupTexts.Length == _textCounter + 1)
+        {
+            PopupText.text = PopupTexts[_textCounter];
+            NextButton.SetActive(false);
+            CloseButton.SetActive(true);
+        } else
+        {
+            PopupText.text = PopupTexts[_textCounter];
+        }
+    }
+
+    public void OnClosePressed()
+    {
+        GameManager.IsPaused = !GameManager.IsPaused;
+        GameManager.CursorIsLocked = !GameManager.CursorIsLocked;
+        Time.timeScale = GameManager.IsPaused ? 0 : 1;
+    }
+
+}

e4874467ac535b4693e7de1a0d1148a963c33aa3
ryanr <ryanrooijakkers@hotmail.com>
2020-12-08 15:10:54 +0100

    Wacky cloud outline code

--- Assets/Scripts/UI/PopupMenu/PopupTrigger.cs
+++ Assets/Scripts/UI/PopupMenu/PopupTrigger.cs
@@ -6,18 +6,38 @@ public class PopupTrigger : MonoBehaviour
 {
     public Canvas Popup;
 
+    public bool IsTriggerForCloud = false;
+
+    public GameObject CloudSnapLocation;
+
+
     private bool _hasBeenTriggered = false;
 
     private void OnTriggerEnter(Collider other)
     {
         if (Popup != null && !_hasBeenTriggered && FindObjectOfType<PlayerBehaviour>() == other.GetComponent<PlayerBehaviour>())
         {
+
+            ActivateCloudOutline();
+
             GameManager.IsPaused = !GameManager.IsPaused;
             GameManager.CursorIsLocked = !GameManager.CursorIsLocked;
             Time.timeScale = GameManager.IsPaused ? 0 : 1;
 
             Popup.gameObject.SetActive(true);
             _hasBeenTriggered = true;
+        }
+    }
+
+    private void ActivateCloudOutline()
+    {
+        if (IsTriggerForCloud && CloudSnapLocation)
+        {
+            Outline outline = CloudSnapLocation.gameObject.GetComponent<Outline>();
+            if (outline)
+            {
+                outline.enabled = true;
+            }
         }
     }
 }

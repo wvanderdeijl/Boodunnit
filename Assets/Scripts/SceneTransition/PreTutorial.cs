8577f481b04153426ca1c265687e0267265648df
ryanr <ryanrooijakkers@hotmail.com>
2020-12-08 13:55:08 +0100

    New Assets

---
+++ Assets/Scripts/SceneTransition/PreTutorial.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreTutorial : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (FindObjectOfType<PlayerBehaviour>() == other.GetComponent<PlayerBehaviour>())
+        {
+            SceneTransitionHandler.Instance.GoToScene("CemetaryScene");
+        }
+    }
+}

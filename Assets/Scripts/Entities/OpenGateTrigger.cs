4c1d95813b2ea8424322b2602265c68f8cee3ae4
ryanr <ryanrooijakkers@hotmail.com>
2020-12-18 14:32:13 +0100

    IGNORE THIS BRANCH

---
+++ Assets/Scripts/Entities/OpenGateTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenGateTrigger : MonoBehaviour
+{
+    public GameObject RatEntityArea;
+
+    private bool _hasBeenTriggered;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!_hasBeenTriggered && FindObjectOfType<PlayerBehaviour>() == other.GetComponent<PlayerBehaviour>())
+        {
+            MoveEntityArea();
+        }
+    }
+
+    private void MoveEntityArea()
+    {
+        if (RatEntityArea)
+        {
+            RatEntityArea.transform.position = new Vector3(-6.96550226f, -40.6840744f, -32.8300018f);
+        }
+    }
+}

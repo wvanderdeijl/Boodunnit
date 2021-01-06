73dec2594e909c182289e44d7e1aae58603336b4
Phone-game <darylvdberg@gmail.com>
2021-01-06 16:15:20 +0100

    Credit scene (Done cheesy)

---
+++ Assets/Scripts/Misc/AutomaticScrolling.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AutomaticScrolling : MonoBehaviour
+{
+
+    public ScrollRect ScrollRect;
+    public RectTransform Content;
+    public float ScrollSpeed;
+    private float MaxScroll;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveContent();
+    }
+
+    private void MoveContent()
+    {
+        Vector2 contentPosition = ScrollRect.content.position;
+        Vector2 newPosition = new Vector2(contentPosition.x, contentPosition.y + ScrollSpeed);
+        ScrollRect.content.position = newPosition;
+    }
+}
---
+++ Assets/Scripts/Misc/EndCredits.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndCredits : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Equals("End"))
+        {
+            print("Done");
+        }
+    }
+}

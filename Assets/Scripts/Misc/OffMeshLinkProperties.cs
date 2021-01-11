f893b552cc2efee69041bfcfa75c6b3ec6522f93
TimvHal <55191080+TimvHal@users.noreply.github.com>
2021-01-11 11:38:43 +0100

    Added default value

--- Assets/Scripts/Misc/OffMeshLinkProperties.cs
+++ Assets/Scripts/Misc/OffMeshLinkProperties.cs
@@ -1,5 +1,14 @@
-public class OffMeshLinkProperties
+using System;
+using UnityEngine;
+
+public class OffMeshLinkProperties : MonoBehaviour
 {
-    public float ParabolaHeight;
-    public float ParabolaDuration;
+    public float ParabolaHeight = 0;
+    public float ParabolaDuration = 0;
+
+    private void Awake()
+    {
+        if (ParabolaHeight == 0f) ParabolaHeight = 5.1f;
+        if (ParabolaDuration == 0f) ParabolaDuration = 2.6f;
+    }
 }
